a2da29decffc431b0c15434395db6e35d241fb4d
Mark Keeble <markkeeble14@gmail.com>
2020-05-05 21:04:31 -0700

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,74 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState, useEffect, useRef } from "react"
+import axios from "axios"
+import TwilioVideo from "twilio-video"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const StartForm = ({storeToken}) => {
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+
+  const handleSubmit = async event => {
+    event.preventDefault()
+
+    const result = await axios({
+        method: 'POST',
+        url: 'https://puce-bobcat-5743.twil.io/-token',
+        data: {
+            identity: name,
+        },
+    })
+
+      const jwt = result.data;
+      storeToken(jwt);
+  }
+    
+  return (
+      <form onSubmit={handleSubmit}>
+          <label htmlFor="name">
+              DisplayName: <br/>
+              <input type='text' id="name" name="name" value = {name} onChange={e => setName(e.target.value)}/>
+          </label>
+          <br/>
+          <label htmlFor="room">
+              Room To Join: <br/>
+              <input type='text' id="room" name="room" value = {room} onChange={e => setRoom(e.target.value)}/>
+          </label>
+          <br/>
+          <button type="submit">Join Video Chat</button>
+      </form>
+  )
+}
+
+const Video = ({token}) => {
+  const localVidRef = useRef()
+  const remoteVidRef = useRef();
+
+  useEffect(() => {
+      TwilioVideo.connect(token, { video: true, audio: true, name: 'test' }).then(result => {
+          console.log('Successfully joined room');
+          console.log(result);
+      })
+  }, [token])
+
+  return  (
+      <div>
+          <div ref={localVidRef}/>
+          <div ref={remoteVidRef}/>
+      </div>
+  )
+}
+
+const IndexPage = () => {
+  const [token, setToken] = useState(false)
+  return (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {!token ? <StartForm storeToken={setToken} /> : <Video token={token}/>}
+    <p> TODO: FUCKKK </p>
   </Layout>
-)
+  )
+}
 
 export default IndexPage
