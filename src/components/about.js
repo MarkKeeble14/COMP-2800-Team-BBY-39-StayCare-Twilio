d542517cb21e39833125f42223c990fd3d77733b
Mark Keeble <markkeeble14@gmail.com>
2020-05-08 12:19:35 -0700

    fixed issue with deploying

--- src/components/about.js
+++ src/components/about.js
@@ -27,9 +27,9 @@ const About = () => {
                 <p> Hey! We're the team behind StayCare, the online daycare! Our goal was to created some kind of service which would help give parents
                     an opporotunity away from their kids while offering a fun, healthy, and interactive form of entertainment.
                     <br/>
+                    <br/>
                     About us specifically, we're all students in BCIT's CST program, which meant that we had a ton to talk about right off the bat. 
                     We hit it off on our first day and developed a friendly team atmosphere built on laughter, learning, and of course, respect. 
-                    <br/>
                     It's been an absolute blast to work with one another on this project and we've all made serious progerss in terms of our web development skills.
                     We cannot wait until we're able to flex our newfound muscles in the field!
                 </p>
