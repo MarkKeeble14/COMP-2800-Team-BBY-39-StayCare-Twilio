10b1f9038d68f95f2c7cf90ffca824860822ebf7
Jonathan Orfani <jonathanorfani@gmail.com>
2020-05-21 11:29:44 -0700

    Added favorites.js and styling to profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -12,6 +12,7 @@ const Profile = () => {
         .then(function (result) {
             if (result.data().isWorker !== undefined) {
                 DisplayProfile(result);
+                addToFavorites(result);
             } else {
                 console.log('you do not have permission to view this profile. You will see your own.');
                 LoadChildren();
@@ -26,6 +27,7 @@ const Profile = () => {
         FillOutProfileData();
     }
 
+    // Check if user is a parent or worker, and display relevant information.
     function DisplayProfile(snap) {
         let role;
         if (!snap.data().isWorker) {
@@ -38,8 +40,26 @@ const Profile = () => {
         $('#display-name').text(snap.data().firstName + ', ' + role);
         $('#email').val(snap.data().email);
         $('#phone-number').val(snap.data().phoneNumber);
+        $("#favoritesDiv").html("<div class='favorites-body'></div>");
+
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user && profile == null) { // When viewing your own profile, display YOUR favorites
+                db.collection("users").doc(user.uid).collection("favorites").get().then(function (snap) {
+                    snap.forEach(function(favorite) {
+                        viewFavorites(favorite);
+                    })
+                })
+            } else if (user && profile != null) { // When viewing someone else's profile, display THEIR favorites
+                db.collection("users").doc(profile).collection("favorites").get().then(function (snap) {
+                    snap.forEach(function(favorite) {
+                        viewFavorites(favorite);
+                    })
+                })
+            }
+        })
     }
     
+    // Get user data from firestore database
     function FillOutProfileData() {
         firebase.auth().onAuthStateChanged(function (user) {
             if (user !== null) {
@@ -51,6 +71,7 @@ const Profile = () => {
         })
     }
     
+    // Load previously added children from database.
     function LoadChildren() {
         firebase.auth().onAuthStateChanged(function (user) {
             if (user !== null) {
@@ -85,10 +106,52 @@ const Profile = () => {
         })
     };
 
+    // Adds child to profile card
     function AddChild(name) {
         $('#child-form').prepend(" <input type='text' class='childname' placeholder='Child' value='" + name + "'></input> ");
     }
 
+    // Add favorite worker information as well as view profile link
+    function viewFavorites(favorite) {
+        $("<h4 class='favorites-title'>" + favorite.data().firstName + "</h4>").appendTo(".favorites-body");
+        $("<p class='favorites-text'> Email: " + favorite.data().email + "</p>").appendTo(".favorites-body");
+        $("<p class='favorites-text'> Phone: " + favorite.data().phoneNumber + "</p>").appendTo(".favorites-body");
+        $("<input type='button' class='btn btn-white btn-animation-1 middled-button' value='View Profile'></input>").appendTo(".favorites-body");
+        $("<hr>").appendTo(".favorites-body");
+
+        // Redirect to worker's profile when you click the "View profile" button
+        $("input").click(function() {
+            window.location.href= "/profile/?profile=" + favorite.data().userKey;
+        })
+    }
+
+    // Add this worker to your personal favorites list
+    function addToFavorites(worker) {
+
+        // Adds userKey, which is the worker's ID used to link to the worker's profile
+        let workerObject = {
+            displayName: worker.data().displayName,
+            email: worker.data().email,
+            firstName: worker.data().firstName,
+            isWorker: worker.data().isWorker,
+            lastName: worker.data().lastName,
+            phoneNumber: worker.data().phoneNumber,
+            photoURL: worker.data().photoURL,
+            userKey: profile
+        }
+
+        // Add to favorites collection
+        $('#favorite-worker').click(function () {
+            firebase.auth().onAuthStateChanged(function (user) {
+                if (user !== null) {
+                    alert('Added ' + workerObject.firstName + ' to favorites! Go to your profile to see your favorite workers.');
+                    db.collection("users").doc(user.uid).collection("favorites").add(workerObject);
+                }
+            })
+        });
+    }
+
+    // HTML for profile card
     return (
         <div id="profile-card">
             <div className="image-container">
@@ -108,6 +171,11 @@ const Profile = () => {
                 </div>
                 <div className="box">
                     <input type="button" className="btn btn-white btn-animation-1 inactive middled-button" id='favorite-worker' value='Favorite This Worker!'/>
+                </div>
+                <div className="box">
+                    <p>Favorite workers</p>
+                    <div id="favoritesDiv">
+                    </div>
                 </div>
                 <div className="child-box inactive">
                     <div className="box">
