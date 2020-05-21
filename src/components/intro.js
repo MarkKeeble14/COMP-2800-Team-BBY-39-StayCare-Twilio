69b2a0ac6856c9258f7beb45a9322821ca360dae
Xieee <justinjayxie@gmail.com>
2020-05-21 11:26:36 -0700

    Adding social media to the website

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import "./css/temp.css"
+import { follow, Follow } from "react-twitter-widgets"
+import { FacebookProvider, Like } from 'react-facebook';
+
+
+
 
 const Intro = () => {
     return (
+        
         <div id="landing">
             <h1 id="landing-title"><b>StayCare</b></h1>
             <p>Integer ultrices tempor orci, ullamcorper faucibus enim ultrices vitae. Proin sit amet scelerisque justo. 
@@ -11,6 +17,23 @@ const Intro = () => {
                 Suspendisse eu facilisis arcu. Maecenas eget lectus sem. Nulla iaculis at neque a rutrum. Phasellus vel maximus velit, sed imperdiet nisi. 
                 Pellentesque dictum quam non euismod eleifend. Morbi rhoncus semper dui, eget consectetur mauris vehicula non.
             </p>
+
+            <h3>Follow us on twitter!</h3>
+
+            <Follow
+                username="StayCareApp"
+                options={{size:"large"}}
+            />
+
+            <h3>Or give us a like on Facebook!</h3>
+
+            <FacebookProvider appId="886355398508956">
+            <Like href="https://www.facebook.com/StayCare-102492371484201/" colorScheme="dark" showFaces share />
+            </FacebookProvider>
+            
+            
+
+           
         </div>
     )
 }
